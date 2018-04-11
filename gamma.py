bbbf665e1c0485609b0128dae7d7775a0c53763c
coulbynguyen <nguyenco@oregonstate.edu>
2018-04-11 15:00:50 -0700

    who knows anymore

--- gamma.py
+++ gamma.py
@@ -15,7 +15,7 @@ streakcountfile = open("streakcount_file.txt", "r")
 
 streaktypefile = open("streaktype_file.txt", "r")
 
-lasthandfile = open("last_hand.txt", "r"
+lasthandfile = open("last_hand.txt", "r")
 
 labels = []
 
@@ -27,22 +27,25 @@ descriptors = []
 
 testdescriptors = []
 
-descriptornames = ["Regular Count", "Player Count", "Bank Count", "Tie Count", "Last Hand Result", "Streak Type", "Streak Count"]
+# descriptornames = ["Regular Count", "Player Count", "Bank Count", "Tie Count", "Last Hand Result", "Streak Type", "Streak Count"]
+descriptornames = ["Player Count", "Streak Count", "Streak Type", "Last Hand Result"]
 
 
 
 
-for x in range(5050):
+
+for x in range(5550):
    result = int(resultsfile.readline())
-   count = int(countfile.readline())
+   # count = int(countfile.readline())
    player = int(playerfile.readline())
-   bank = int(bankfile.readline())
-   tie = int(tiefile.readline())
+   # bank = int(bankfile.readline())
+   # tie = int(tiefile.readline())
    streakcount = int(streakcountfile.readline())
    streaktype = int(streaktypefile.readline())
    lasthand = int(lasthandfile.readline())
-   oneset = [count,player,bank,tie,lasthand,streaktype,streakcount]
-   if x < 5000:
+   # oneset = [count,player,bank,tie,lasthand,streaktype,streakcount]
+   oneset = [player, streakcount, streaktype, lasthand]
+   if x < 5500:
       descriptors.append(oneset)
       labels.append(result)
    else:
@@ -52,11 +55,19 @@ for x in range(5050):
 clf = tree.DecisionTreeClassifier()
 clf.fit(descriptors, labels)
 
+predictions = clf.predict(testdescriptors)
+
 print (testlabels)
 print ("\n")
 print (clf.predict(testdescriptors))
 
-import graphviz
-dot_data = tree.export_graphviz(clf, out_file=None, feature_names=descriptornames, class_names=labelnames, filled=True, rounded=True, special_characters=True, impurity=False)
-graph = graphviz.Source(dot_data)
-graph.render("Baccarat")
+from sklearn.metrics import accuracy_score
+print (accuracy_score(testlabels, predictions))
+
+
+
+# below is the code that creates the graph but right now i need to work on finding out what data is relevant
+# import graphviz
+# dot_data = tree.export_graphviz(clf, out_file=None, feature_names=descriptornames, class_names=labelnames, filled=True, rounded=True, special_characters=True, impurity=False)
+# graph = graphviz.Source(dot_data)
+# graph.render("Baccarat")
